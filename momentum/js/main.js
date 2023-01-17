53aaf7a3e9f46e8266e72fef449952de98c8aaae
Wystov <wystov@gmail.com>
2023-01-17 20:41:43 +0300

    fix: greeting get time

--- momentum/js/main.js
+++ momentum/js/main.js
@@ -1,19 +1,20 @@
 const time = document.querySelector('.time');
 const date = document.querySelector('date');
 const dateOptions = {weekday: 'long', day: 'numeric', month: 'long'};
+let newDate = new Date();
+let currentTime = newDate.toLocaleTimeString();
 const greeting = document.querySelector('.greeting');
 const name = document.querySelector('.name');
 name.addEventListener('blur', setLocalStorage);
-name.addEventListener("keydown", keyDown);
+name.addEventListener("keydown", nameBlur);
+window.addEventListener('beforeunload', setLocalStorage);
 window.addEventListener('load', getLocalStorage);
 
 
-
 function showTime() {
-    const newDate = new Date();
+    newDate = new Date();
     const currentTime = newDate.toLocaleTimeString();
     time.textContent = currentTime;
-    const hours = newDate.getHours();
     
     showDate()
     showGreeting()
@@ -22,19 +23,18 @@ function showTime() {
 }
 
 function showDate() {
-    const newDate = new Date();
     const currentDate = newDate.toLocaleDateString('ru-RU', dateOptions);
     date.textContent = currentDate;
 }
 
 function getTimeOfDay() {
-    const newDate = new Date();
-    const currentTime = newDate.toLocaleTimeString();
-    if (currentTime >= 0 && currentTime <= 3) {
+    const hours = newDate.getHours();
+    console.log(hours);
+    if (hours >= 0 && hours <= 3) {
         return 'Night';
-    } else if (currentTime >= 4 && currentTime <= 11) {
+    } else if (hours >= 4 && hours <= 11) {
         return 'Morning';
-    } else if (currentTime >= 12 && currentTime <= 17) {
+    } else if (hours >= 12 && hours <= 17) {
         return 'Day';
     } else {
         return 'Evening';
@@ -52,13 +52,13 @@ function setLocalStorage() {
 }
 
 function getLocalStorage() {
-    if(localStorage.getItem('name')) {
+    if (localStorage.getItem('name')) {
       name.value = localStorage.getItem('name');
       name.blur();
     }
 }
 
-function keyDown (key) {
+function nameBlur (key) {
     if (key.keyCode === 13) {
       name.blur();
     }

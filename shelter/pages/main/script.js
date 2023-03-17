21d2d7acc017ca5378247ed8669097a98d2bf550
Wystov <wystov@gmail.com>
2023-03-17 21:46:32 +0300

    feat: partial slider

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,20 +1,84 @@
-console.log(`Все пункты тз выполнены.
-100/100 баллов.
-1. Вёрстка страницы Main соответствует макету при ширине экрана 1280px +14
-2. Вёрстка страницы Main соответствует макету при ширине экрана 768px +14
-3. Вёрстка страницы Main соответствует макету при ширине экрана 320px +14
-4. Вёрстка страницы Pets соответствует макету при ширине экрана 1280px +6
-5. Вёрстка страницы Pets соответствует макету при ширине экрана 768px +6
-6. Вёрстка страницы Pets соответствует макету при ширине экрана 320px +6
-7. Ни на одном из разрешений до 320px включительно не появляется горизонтальная
-   полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +20
-8. Верстка резиновая: при плавном изменении размера экрана элементы верстки
-   так же плавно меняют свои размеры и расположение, не наезжают друг на друга, 
-   изображения могут менять размер, но сохраняют правильные пропорции +8
-9. При ширине экрана меньше 768px на обеих страницах меню в хедере скрывается,
-   появляется иконка бургер-меню +4
-   Открытие меню при клике на иконку бургер-меню на текущем этапе не проверяется
-10. Верстка обеих страниц валидная: для проверки валидности вёрстки 
-    используйте сервис https://validator.w3.org/ . +8
+console.log(`
+
 `)
+
+const btnLeft = document.querySelector('.slider__left');
+const btnRight = document.querySelector('.slider__right');
+const container = document.querySelector('.slider__content');
+const prevPage = document.querySelector('.prev-page');
+const currentPage = document.querySelector('.current-page');
+const nextPage = document.querySelector('.next-page');
+
+const contentPrev = new Set;
+const contentCurrent = new Set;
+const contentNext = new Set;
+
+
+
+
+// CARD CONTENT
+
+function getRandom(min, max) {
+   return Math.floor(Math.random() * (max - min) + min);
+}
+
+function createCards(petsData, page, set) {
+   const random = getRandom(0, 8);
+   if (!contentPrev.has(petsData[random]) &&
+      !contentCurrent.has(petsData[random]) &&
+      !contentNext.has(petsData[random])) {
+      set.add(petsData[random]);
+      const card = document.createElement('div');
+      card.classList.add('pets__card', 'card');
+      const cardImg = document.createElement('div');
+      cardImg.classList.add('card__img', `${petsData[random].img}`);
+      const cardName = document.createElement('div');
+      cardName.classList.add('card__name');
+      cardName.textContent = `${petsData[random].name}`;
+      const button = document.createElement('a');
+      button.classList.add('card__button', 'button');
+      button.textContent = 'Learn more';
+      card.append(cardImg, cardName, button);
+      page.append(card);
+   } else {
+      createCards(petsData, page, set)
+   }
+}
+
+
+async function getData() {
+   const data = '../../assets/data/pets.json';
+   const res = await fetch(data);
+   const petsData = await res.json();
+
+   for (let i = 0; i < 3; i++) {
+      createCards(petsData, prevPage, contentPrev);
+   }
+
+   for (let i = 0; i < 3; i++) {
+      createCards(petsData, currentPage, contentCurrent);
+   }
+
+   for (let i = 0; i < 3; i++) {
+      createCards(petsData, nextPage, contentNext);
+   }
+}
+
+getData()
+
+
+// SLIDER
+
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+
+function moveLeft() {
+   container.classList.add('slider__content--move-left');
+   setTimeout(() => container.classList.remove('slider__content--move-left'), 600)
+}
+
+function moveRight() {
+   container.classList.add('slider__content--move-right');
+   setTimeout(() => container.classList.remove('slider__content--move-right'), 600)
+}
 

1a17aadf4084b614a98a0aecb52355bf550922ad
Wystov <wystov@gmail.com>
2023-03-17 23:13:04 +0300

    fix: slider works correctly

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -9,12 +9,11 @@ const prevPage = document.querySelector('.prev-page');
 const currentPage = document.querySelector('.current-page');
 const nextPage = document.querySelector('.next-page');
 
-const contentPrev = new Set;
-const contentCurrent = new Set;
-const contentNext = new Set;
+let petsData;
 
-
-
+let contentPrev = [];
+let contentCurrent = [];
+let contentNext = [];
 
 // CARD CONTENT
 
@@ -22,12 +21,10 @@ function getRandom(min, max) {
    return Math.floor(Math.random() * (max - min) + min);
 }
 
-function createCards(petsData, page, set) {
+function createCards(petsData, page, set, nextSet) {
    const random = getRandom(0, 8);
-   if (!contentPrev.has(petsData[random]) &&
-      !contentCurrent.has(petsData[random]) &&
-      !contentNext.has(petsData[random])) {
-      set.add(petsData[random]);
+   if (!set.includes(random) && !nextSet.includes(random)) {
+      set.push(random);
       const card = document.createElement('div');
       card.classList.add('pets__card', 'card');
       const cardImg = document.createElement('div');
@@ -41,7 +38,7 @@ function createCards(petsData, page, set) {
       card.append(cardImg, cardName, button);
       page.append(card);
    } else {
-      createCards(petsData, page, set)
+      createCards(petsData, page, set, nextSet)
    }
 }
 
@@ -49,18 +46,10 @@ function createCards(petsData, page, set) {
 async function getData() {
    const data = '../../assets/data/pets.json';
    const res = await fetch(data);
-   const petsData = await res.json();
+   petsData = await res.json();
 
    for (let i = 0; i < 3; i++) {
-      createCards(petsData, prevPage, contentPrev);
-   }
-
-   for (let i = 0; i < 3; i++) {
-      createCards(petsData, currentPage, contentCurrent);
-   }
-
-   for (let i = 0; i < 3; i++) {
-      createCards(petsData, nextPage, contentNext);
+      createCards(petsData, currentPage, contentCurrent, contentNext);
    }
 }
 
@@ -73,12 +62,53 @@ btnLeft.addEventListener('click', moveLeft);
 btnRight.addEventListener('click', moveRight);
 
 function moveLeft() {
+   if (contentPrev.length) {
+
+   } else {
+      contentPrev = [];
+      for (let i = 0; i < 3; i++) {
+         createCards(petsData, prevPage, contentPrev, contentCurrent);
+      }
+   }
+
    container.classList.add('slider__content--move-left');
-   setTimeout(() => container.classList.remove('slider__content--move-left'), 600)
+   btnLeft.removeEventListener('click', moveLeft);
+
+   setTimeout(() => {
+      nextPage.innerHTML = currentPage.innerHTML;
+      contentNext = [...contentCurrent];
+      currentPage.innerHTML = prevPage.innerHTML;
+      contentCurrent = [...contentPrev];
+      prevPage.innerHTML = '';
+      contentPrev = [];
+      container.classList.remove('slider__content--move-left')
+      btnLeft.addEventListener('click', moveLeft);
+      console.log(contentPrev, contentCurrent, contentNext)
+   }, 800)
 }
 
 function moveRight() {
+   if (contentNext.length) {
+
+   } else {
+      contentNext = [];
+      for (let i = 0; i < 3; i++) {
+         createCards(petsData, nextPage, contentNext, contentCurrent);
+      }
+   }
+
    container.classList.add('slider__content--move-right');
-   setTimeout(() => container.classList.remove('slider__content--move-right'), 600)
+   btnRight.removeEventListener('click', moveRight);
+   setTimeout(() => {
+      prevPage.innerHTML = currentPage.innerHTML;
+      contentPrev = [...contentCurrent];
+      currentPage.innerHTML = nextPage.innerHTML;
+      contentCurrent = [...contentNext];
+      nextPage.innerHTML = '';
+      contentNext = [];
+      container.classList.remove('slider__content--move-right')
+      btnRight.addEventListener('click', moveRight);
+      console.log(contentPrev, contentCurrent, contentNext)
+   }, 800)
 }
 

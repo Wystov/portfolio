08523e75571dde40978888a3ab5ac946ec6c8c4b
Wystov <wystov@gmail.com>
2023-03-20 22:30:54 +0300

    refactor: popup to module

---
+++ shelter/assets/js/popup.js
@@ -0,0 +1,33 @@
+import { overlay } from "../../assets/js/burger.js";
+const popUp = document.querySelector('.pop-up');
+const popUpImg = document.querySelector('.pop-up__img');
+const popUpName = document.querySelector('.pop-up__name');
+const popUpType = document.querySelector('.pop-up__type');
+const popUpDescription = document.querySelector('.pop-up__description');
+const popUpAge = document.querySelector('.pop-up__age');
+const popUpInoculations = document.querySelector('.pop-up__inoculations');
+const popUpDiseases = document.querySelector('.pop-up__diseases');
+const popUpParasites = document.querySelector('.pop-up__parasites');
+const popUpCloseBtn = document.querySelector('.pop-up__close-btn');
+
+function showPopup(content) {
+    popUpImg.classList.add(`${content.img}`);
+    popUpName.textContent = content.name;
+    popUpType.textContent = content.type;
+    popUpDescription.textContent = content.description;
+    popUpAge.textContent = content.age;
+    popUpInoculations.textContent = content.inoculations;
+    popUpDiseases.textContent = content.diseases;
+    popUpParasites.textContent = content.parasites;
+
+    popUp.classList.add('pop-up--active');
+    overlay.classList.add('overlay--active');
+}
+
+function closePopUp() {
+    popUp.classList.remove('pop-up--active');
+    popUpImg.classList.remove(`${popUpImg.classList[1]}`)
+    overlay.classList.remove('overlay--active');
+}
+
+export { popUp, popUpImg, popUpName, popUpType, popUpDescription, popUpAge, popUpInoculations, popUpDiseases, popUpParasites, popUpCloseBtn, showPopup, closePopUp }
--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -1,4 +1,5 @@
-import { burgerBtn, nav, navLinks, logo, overlay, toggleNav } from '../../assets/js/burger.js'
+import { burgerBtn, nav, navLinks, overlay, toggleNav } from '../../assets/js/burger.js'
+import { popUp, popUpCloseBtn, showPopup, closePopUp } from '../../assets/js/popup.js';
 
 const btnToStart = document.querySelector('.slider-btns__double-back');
 const btnToEnd = document.querySelector('.slider-btns__double-forward');
@@ -148,42 +149,8 @@ function deactivateArrows() {
 
 // POPUP
 
-const popUp = document.querySelector('.pop-up');
-const popUpImg = document.querySelector('.pop-up__img');
-const popUpName = document.querySelector('.pop-up__name');
-const popUpType = document.querySelector('.pop-up__type');
-const popUpDescription = document.querySelector('.pop-up__description');
-const popUpAge = document.querySelector('.pop-up__age');
-const popUpInoculations = document.querySelector('.pop-up__inoculations');
-const popUpDiseases = document.querySelector('.pop-up__diseases');
-const popUpParasites = document.querySelector('.pop-up__parasites');
-
-
-const popUpCloseBtn = document.querySelector('.pop-up__close-btn');
 popUpCloseBtn.addEventListener('click', closePopUp);
 overlay.addEventListener('click', () => {
    if (popUp.classList.contains('pop-up--active')) closePopUp();
    if (nav.classList.contains('nav--active')) toggleNav();
 })
-
-
-
-function showPopup(content) {
-   popUpImg.classList.add(`${content.img}`);
-   popUpName.textContent = content.name;
-   popUpType.textContent = content.type;
-   popUpDescription.textContent = content.description;
-   popUpAge.textContent = content.age;
-   popUpInoculations.textContent = content.inoculations;
-   popUpDiseases.textContent = content.diseases;
-   popUpParasites.textContent = content.parasites;
-
-   popUp.classList.add('pop-up--active');
-   overlay.classList.add('overlay--active');
-}
-
-function closePopUp() {
-   popUp.classList.remove('pop-up--active');
-   popUpImg.classList.remove(`${popUpImg.classList[1]}`)
-   overlay.classList.remove('overlay--active');
-}

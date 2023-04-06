608b0a91712164a9da6c687547369812421b32de
Wystov <wystov@gmail.com>
2023-04-07 00:01:56 +0300

    fix: adjustments to match task creteria part3

--- shelter/assets/js/burger.js
+++ shelter/assets/js/burger.js
@@ -2,13 +2,11 @@ const burgerBtn = document.querySelector('.burger');
 const nav = document.querySelector('.nav');
 const navLinks = document.querySelectorAll('.nav__link');
 const overlay = document.querySelector('.overlay');
-const logo = document.querySelector('.logo');
 
 function toggleNav() {
     nav.classList.toggle('nav--active');
     burgerBtn.classList.toggle('burger--active');
     overlay.classList.toggle('overlay--active');
-    logo.classList.toggle('logo--active');
     document.body.classList.toggle('no-scroll');
     burgerLinksClick()
 }
@@ -21,4 +19,4 @@ function burgerLinksClick() {
     }
 }
 
-export { burgerBtn, nav, navLinks, logo, overlay, toggleNav };
+export { burgerBtn, nav, navLinks, overlay, toggleNav };

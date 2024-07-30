a26cbb188a06bf78f6974ec38483c8c8f4272f53
Wystov <wystov@gmail.com>
2024-07-30 08:42:13 +0200

    fix: animated text crop

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    fontFamily: {
+      poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],

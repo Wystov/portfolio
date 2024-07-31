5a421abed7dd1acee3babe21b60de177c42de7df
Wystov <wystov@gmail.com>
2024-07-31 18:08:00 +0200

    fix: styles of tag

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -32,10 +32,12 @@ export const Projects = ({ data }: Props) => {
     <section class="mt-6">
       <div class="grid grid-cols-6 gap-4">
         <div class="col-span-6 sm:col-span-1">
-          <div class="flex justify-between">
-            <p class="mb-2">Filter</p>
+          <div class="flex justify-between mb-2">
+            <p>Filter</p>
             <Show when={activeTags().length}>
-              <button onClick={() => setTags(mapTagsState(data))}>reset</button>
+              <button onClick={() => setTags(mapTagsState(data))} class="hover:opacity-70">
+                reset
+              </button>
             </Show>
           </div>
           <div class="flex flex-wrap gap-2 flex-row sm:flex-col">
@@ -43,7 +45,7 @@ export const Projects = ({ data }: Props) => {
               <button
                 onClick={() => toggleTag(tag)}
                 disabled={!filteredTags().includes(tag)}
-                class="flex justify-start items-center gap-2 cursor-pointer disabled:opacity-50 disabled:cursor-auto rounded-md p-2 bg-slate-700/80 hover:bg-slate-700 w-full"
+                class="flex justify-start items-center gap-2 cursor-pointer disabled:opacity-50 disabled:cursor-auto rounded-md p-2 bg-slate-700/80 hover:bg-slate-700 sm:w-full"
               >
                 <svg class="size-5">
                   <use

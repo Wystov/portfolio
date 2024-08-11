3091e31ec3f9e8c0e91a9dc15bc0ec165af7a4bc
Wystov <wystov@gmail.com>
2024-08-11 13:26:30 +0200

    feat: style project card

--- src/components/Projects/ProjectList/Card/index.tsx
+++ src/components/Projects/ProjectList/Card/index.tsx
@@ -9,22 +9,29 @@ export const Card = (props: Props) => {
   return (
     <a
       href={`/projects/${props.project.slug}/`}
-      class="col-span-2 flex rounded border-2 p-2 md:col-span-1"
+      class="group col-span-2 flex flex-col rounded border-2 border-white/20 p-4 duration-300 hover:border-amber-300 md:col-span-1"
     >
-      <div>
-        <h3>{props.project.data.title}</h3>
-        <p>
-          {props.project.data.date.toLocaleDateString('en-US', {
-            day: 'numeric',
-            month: 'short',
-            year: 'numeric',
-          })}
-        </p>
-        <p>{props.project.data.brief}</p>
-        <ul class="flex flex-wrap gap-x-2 gap-y-1">
-          <For each={props.project.data.tags}>{(tag) => <li>{tag}</li>}</For>
-        </ul>
-      </div>
+      <p class="relative inline-flex text-3xl font-bold">
+        <span class="absolute -translate-x-4 text-amber-300 opacity-0 duration-300 group-hover:translate-x-0 group-hover:opacity-100">
+          {'> '}
+        </span>
+        <span class="duration-300 group-hover:translate-x-6">
+          {props.project.data.title}
+        </span>
+      </p>
+      <p class="mb-2 text-sm opacity-70">
+        {props.project.data.date.toLocaleDateString('en-US', {
+          day: 'numeric',
+          month: 'short',
+          year: 'numeric',
+        })}
+      </p>
+      <p class="mb-4 text-lg">{props.project.data.brief}</p>
+      <ul class="mt-auto flex flex-wrap gap-x-2 gap-y-1">
+        <For each={props.project.data.tags}>
+          {(tag) => <li class="rounded bg-slate-500/50 p-1 text-sm">{tag}</li>}
+        </For>
+      </ul>
     </a>
   );
 };

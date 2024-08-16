61c3dcfa2cf9edc988830bcd08e4f10435d6fc0a
Wystov <wystov@gmail.com>
2024-08-16 07:02:01 +0200

    feat: configure tailwind theme with css vars

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,10 +6,16 @@ import twTypographyPlugin from '@tailwindcss/typography';
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    fontFamily: {
-      poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        neutral: 'rgb(var(--color-bg) / <alpha-value>)',
+        primary: 'rgb(var(--color-text) / <alpha-value>)',
+        accent: 'rgb(var(--color-accent) / <alpha-value>)',
+      },
     },
-    extend: {},
   },
   plugins: [twTypographyPlugin],
   darkMode: 'selector',
